c1c4e60a965f6f38ba46374f82f632ae1b382571
tunge2 <89797448+tunge2@users.noreply.github.com>
2021-12-16 22:34:01 -0500

    Update and rename DOdobot (1).m to DOdobot.m

--- DOdobot.m
+++ DOdobot.m
@@ -1,3 +1,5 @@
+% Moves DoBot using moveJoint method
+
 function time = DOdobot(angles, arduinoObj)
 tstart = tic;
 
@@ -5,18 +7,18 @@ isGrab = 0; % No suction
 
 % Set the float values in the data package
 line_float_cmd = zeros(1,10); 
-% CARTESIAN = 3;
+% CARTESIAN  = 3;
 % JOINT = 6;
 line_float_cmd(1) = 6; %JOINT;
-line_float_cmd(3) = angles(1);
-line_float_cmd(4) = angles(2);
-line_float_cmd(5) = angles(3);
+line_float_cmd(3) = angles(1); %joint 1 (degrees)
+line_float_cmd(4) = angles(2); %joint 2 (degrees)
+line_float_cmd(5) = angles(3); %joint 3 (degrees)
 %line_float_cmd(6) = angles(4);
 line_float_cmd(7) = isGrab;
 % MOVE_MODE_JUMP = 0; % moveJump
 % MOVE_MODE_JOINTS = 1;  % joints move independent
 % MOVE_MODE_LINEAR = 2;  % linear movement
-line_float_cmd(8) = 1; %MOVE_MODE N
+line_float_cmd(8) = 1; %MOVE_MODE N %moveJoint 
 
 % Combine datapackage
 header_chr = char(hex2dec('A5'));
